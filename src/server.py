56e216c6839d20bb1632c46b8a6433ab4f53f66e
Denny Herbrich <denny.herbrich@gmail.com>
2024-06-21 12:28:18 +0200

    proxyfix

--- src/server.py
+++ src/server.py
@@ -5,7 +5,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from loggingmiddleware import LoggingMiddleware
 
 app = Flask(__name__)
-app.wsgi_app = LoggingMiddleware(ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_prefix=1))
+app.wsgi_app = LoggingMiddleware(ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1))
 
 
 @app.route('/')
